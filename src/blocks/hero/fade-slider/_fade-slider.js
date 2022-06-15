993de2a3359a5c8a7e04aeb89426136e3b24b1e1
VladimiraRe <vladimira_ra@mail.ru>
2022-06-15 15:09:49 +0300

    Created animation for slider.

---
+++ src/blocks/hero/fade-slider/_fade-slider.js
@@ -0,0 +1,14 @@
+const slides = document.querySelectorAll('.fade-slider__item');
+let activeClass = 'fade-slider__item--visible';
+let index = 0;
+
+setInterval(function() {
+    slides[index].classList.remove(activeClass);
+    index++;
+
+    if (index + 1 > slides.length) {
+        index = 0;
+    }
+
+    slides[index].classList.add(activeClass);
+}, 5000);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 //Подключение стилей
-import "./css/style.scss";
-
+import './css/style.scss';
 //Подключение скрипта сайдбара
-import "./blocks/sidebar/_sidebar";
+import './blocks/sidebar/_sidebar';
+//Подключение скрипта слайдера
+import './blocks/hero/fade-slider/_fade-slider';
